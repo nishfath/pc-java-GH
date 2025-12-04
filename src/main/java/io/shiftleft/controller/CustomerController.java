0f0cc7f69092350b024d9b98d2549ba2d34bce2c
Qwiet AI <noreply@shiftleft.io>
2025-12-03 20:42:55 -0500

    Fixing src/main/java/io/shiftleft/controller/CustomerController.java for finding 3

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -277,34 +277,36 @@ public Customer getCustomer(@PathVariable("customerId") Long customerId) {
    * @param phoneNumber String
    * @param httpResponse
    * @param request
-   * @return String
-   * @throws IOException
-   */
-@RequestMapping(value = "/debug", method = RequestMethod.GET)
+@RequestMapping(value = "/debug", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
 public String debug(@RequestParam String customerId,
-                  @RequestParam int clientId,
-                  @RequestParam String firstName,
-                  @RequestParam String lastName,
-                  @RequestParam String dateOfBirth,
-                  @RequestParam String ssn,
-                  @RequestParam String socialSecurityNum,
-                  @RequestParam String tin,
-                  @RequestParam String phoneNumber,
-                  HttpServletResponse httpResponse,
-                 WebRequest request) throws IOException{
+                    @RequestParam int clientId,
+                    @RequestParam String firstName,
+                    @RequestParam String lastName,
+                    @RequestParam String dateOfBirth,
+                    @RequestParam String ssn,
+                    @RequestParam String socialSecurityNum,
+                    @RequestParam String tin,
+                    @RequestParam String phoneNumber,
+                    HttpServletResponse httpResponse,
+                    WebRequest request) throws IOException {
 
     // empty for now, because we debug
     Set<Account> accounts1 = new HashSet<Account>();
     //dateofbirth example -> "1982-01-10"
     Customer customer1 = new Customer(customerId, clientId, firstName, lastName, DateTime.parse(dateOfBirth).toDate(),
-                                  ssn, socialSecurityNum, tin, phoneNumber, new Address("Debug str",
-                                  "", "Debug city", "CA", "12345"),
-                                  accounts1);
+                                    ssn, socialSecurityNum, tin, phoneNumber, new Address("Debug str",
+                                    "", "Debug city", "CA", "12345"),
+                                    accounts1);
 
     customerRepository.save(customer1);
     httpResponse.setStatus(HttpStatus.CREATED.value());
     httpResponse.setHeader("Location", String.format("%s/customers/%s",
-                       request.getContextPath(), customer1.getId()));
+                         request.getContextPath(), customer1.getId()));
+
+    // Properly encode customer data before returning to prevent XSS attacks
+    return Encode.forHtml(customer1.toString());
+}
+
 
     // Apply proper HTML escaping to prevent XSS attacks
     return HtmlUtils.htmlEscape(customer1.toString());
